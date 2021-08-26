3e6d98a14494752b20135a53b1566bd3433e7fe2
Ananya157 <54083802+Ananya157@users.noreply.github.com>
2021-08-26 10:30:22 -0500

    Update contacts.php

--- api/contacts.php
+++ api/contacts.php
@@ -1,68 +1,50 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+
 $host = "mysql"; 
 $user = "AataDB21"; 
-$password = "Aata2021@"; 
+$password = "AataDB21@"; 
 $dbname = "db_aata"; 
-$port = 3306;
 $email = '';
 
-$con = mysqli_connect($host, $user, $password,$dbname, $port);
-
+$con = mysqli_connect($host, $user, $password,$dbname);
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-
 
 if (!$con) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-
 switch ($method) {
     case 'GET':
-      $email = $_GET['email'];
-      $sql = "select * from Members".($email?" where email=$email":''); 
+      $sql = "select * from `Members`"; 
+      mysqli_query($con, $sql);
+      echo '[';
+      for ($i=0 ; $i<mysqli_num_rows($result) ; $i++) {
+        echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+      }
+      echo ']';
       break;
     case 'POST':
-      $name = $_POST["name"];
-      $spouseName = $_POST["spouseName"];
-      $address = $_POST["address"];
-      $city = $_POST["city"];
-      $state = $_POST["state"];
-      $zip = $_POST["zip"];
-      $memType = $_POST["memType"];
-      $email = $_POST["email"];
-      $phone = $_POST["phone"];
-      $pMethod = $_POST["pMethod"];
-      $query = "SELECT email FROM Members WHERE email =$email";
-      $result = $database->query($query);
-      if($result ->num_rows){
+      $name = $_POST['name'];
+      $spouseName = $_POST['spouseName'];
+      $address = $_POST['address'];
+      $city = $_POST['city'];
+      $state = $_POST['state'];
+      $zip = $_POST['zipcode'];
+      $memType = $_POST['type'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
+      $pMethod = $_POST['pay'];
+      $query = "SELECT Name FROM `Members` WHERE Email = '$email'  and Name = '$name' "; 
+      $result = mysqli_query($con, $query);
+      if($result ->num_rows > 0){
           echo "Email already exists in our database.";
           break;
       }else{
-          $sql = "insert into Members (Name,SpouseName, Address, City, State, ZIP, MemType, Email, Phone, P-Method) values ('$name', '$spouseName', '$address', '$city', '$state', '$zip', '$memType', '$email', '$phone', '$pMethod')";
+          $sql = "INSERT INTO `Members`(`Name`, `SpouseName`, `Address`, `City`, `ZiP`, `Email`, `Phone`, `P-Method`, `State`, `MemType`) values ('$name', '$spouseName', '$address', '$city', '$zip', '$email', '$phone', '$pMethod', '$state', '$memType')"; 
+          mysqli_query($con, $sql);
+          echo "Data Added";
           break; 
       }
 }
-
-// run SQL statement
-$result = mysqli_query($con,$sql);
-
-// die if SQL statement failed
-if (!$result) {
-  http_response_code(404);
-  die(mysqli_error($con));
-}
-
-if ($method == 'GET') {
-    if (!$email) echo '[';
-    for ($i=0 ; $i<mysqli_num_rows($result) ; $i++) {
-      echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
-    }
-    if (!$email) echo ']';
-  } elseif ($method == 'POST') {
-    echo json_encode($result);
-  } else {
-    echo mysqli_affected_rows($con);
-  }
-
-$con->close();
