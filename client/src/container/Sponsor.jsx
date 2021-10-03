32df0be32488e2c977b50a07fd740f068c05e7f2
Ananya157 <54083802+Ananya157@users.noreply.github.com>
2021-10-02 19:25:57 -0500

    Update Sponsor.jsx

--- client/src/container/Sponsor.jsx
+++ client/src/container/Sponsor.jsx
@@ -10,7 +10,7 @@ export const SponsorPage = () => {
             <h1 className="homeHeader newsletter border">
                 Sponsorship
                 </h1>
-            <h1 className="homeHeader tuluLipi marginBorder">
+            <h1 className="homeHeader tuluLipi sponsorMessage">
                 Sponsored Picture Greeting: $75 per greeting
                 <br />
                 Sponsored Crawl Greeting: $25 per greeting
@@ -18,6 +18,9 @@ export const SponsorPage = () => {
                 Business Sponsoring - Banner: $250 per greeting
                 <br />
                 Business Sponsoring - Crawl: $75 per greeting
+            </h1>
+            <h1 className="homeHeader tuluLipi marginBorder">
+                Please send/share family pictures /banners to : <a href="mailto:aata.channel@gmail.com" target="_blank" rel="noreferrer">Email Us(aata.channel@gmail.com)</a> 
             </h1>
             <TuluForm whichpage={page} />
         </div>
