d14d9f9adaaa104308f3e37532d54102d3964f24
Ananya157 <54083802+Ananya157@users.noreply.github.com>
2021-10-07 13:42:45 -0500

    Update ContactUs.jsx

--- client/src/container/ContactUs.jsx
+++ client/src/container/ContactUs.jsx
@@ -1,7 +1,17 @@
+import "../styles/home.scss"
+
 export const ContactUs = () => {
     return (
         <div>
-            Content will be added  soon
+            <h1 className="homeHeader newsletter marginBorder">
+                Contact Us
+            </h1>
+
+            <h1 className="homeHeader tuluLipi marginBorder">
+                <b>Address</b>: All America Tulu Association 2 Atwood Ln Andover MA 01810
+                <br/>
+                <b>Email</b>:  <a href="mailto:aata.channel@gmail.com" target="_blank" rel="noreferrer">Email Us(aata.channel@gmail.com)</a> 
+             </h1>
         </div>
     )
 }
